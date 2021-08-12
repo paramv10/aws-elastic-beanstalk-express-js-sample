078e7bab36eb48628297179442dfcae393c9397e
param <param01@gmail.com>
2021-08-12 11:18:33 +0530

    commit changes into app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('trying to commit something new - learnig how to do forking and pushing onto GitRepository.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
